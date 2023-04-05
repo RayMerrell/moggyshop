8a8cabc141ddc249a30133570b9c76a0d0be2738
Ray Merrell <RayMerrell@outlook.com>
2023-04-05 22:35:53 +0100

    more data

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -1,22 +1,27 @@
 const CatCard = (catData) => {
-    return (
-        // <div>hello world cat card</div>
-        <div className="row">
-            <div className="col s12 m7">
-                <div className="card small">
-                    <div className="card-image">
-                        <img src={catData.url}></img>
-                        <span className="card-title">{catData.name} for sale!</span>
-                    </div>
-                    <div className="card-content">
-                        <p>{catData.country}</p><p>{catData.temperament}</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">Buy this moggy</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-} 
+  return (
+    // <div>hello world cat card</div>
+    <div className="shadowCard card small">
+      <div className="card-image waves-effect waves-block waves-light">
+        <img className="activator mainCatPic" src={catData.url} />
+      </div>
+      <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">
+          {catData.name} the {catData.breed}<i className="material-icons right">{catData.temperament}</i>
+        </span>
+        <p>
+          <a href="#">This is a link</a>
+        </p>
+      </div>
+      <div className="card-reveal">
+        <span className="card-title grey-text text-darken-4">
+        {catData.breed}<i className="material-icons right">close</i>
+        </span>
+        <p>
+          {catData.description}
+        </p>
+      </div>
+    </div>
+  );
+};
 export default CatCard;
