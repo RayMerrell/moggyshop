d9aa852239a2a19641289dd1781e9d567d879d13
Ray Merrell <RayMerrell@outlook.com>
2023-04-06 17:44:42 +0100

    Name and price made so they're consistant

--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -1,9 +1,9 @@
 const NavFooter = () => {
     return (
         <nav>
-            <div class="nav-wrapper green">
-                <a href="" class="brand-logo center">Logo</a>
-                <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <div className="nav-wrapper green">
+                <a href="" className="brand-logo center">Logo</a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
                     <li><a href="#">Dogs2Door</a></li>
                     <li><a href="#">Horses2Home</a></li>
                     <li><a href="#">Contact</a></li>
