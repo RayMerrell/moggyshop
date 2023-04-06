8532f059d4358532e8991cc66a95a058b3c4419c
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-06 17:37:46 +0100

    1111

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@
 const Navbar = () => {
     return (
         <nav>
-        <div class="valign-wrapper pink">
-          <a href="" class="brand-logo center">Cats 4 You</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <div className="valign-wrapper pink">
+          <a href="" className="brand-logo center">Cats 4 You</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="">Home</a></li>
             <li><a href="">Contact</a></li>
             <li><a href="">About</a></li>
           </ul>
           
-          <a class="btn blue"><i class="material-icons right">shopping_cart</i>button</a>
+          <a className="btn blue"><i className="material-icons right">shopping_cart</i>button</a>
 
-            <span class="new badge blue">0</span>
+            <span className="new badge blue">0</span>
         </div>
       </nav>
 
